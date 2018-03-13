a9aa562018248566e7c0cf45cb7f3e2094242e7f
Pedro Jesus <pedrojesus.cefet@gmail.com>
2018-03-13 16:10:06 -0300

    Adicionar arquivos de projeto.

---
+++ FormExemplo/Controllers/MessagesController.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using FormExemplo.Modelos;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+
+namespace FormExemplo
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity,Dialogo);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+        public static int campos;
+        internal static IDialog<object> Dialogo()
+        {            
+            return Chain.From(()=>FormDialog.FromForm(Exemplo.BuildForm));
+        }
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ FormExemplo/Modelos/Exemplo.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace FormExemplo.Modelos
+{
+    [Serializable]
+    [Template(TemplateUsage.NotUnderstood, "Desculpe não entendi \"{0}\".")]
+    //[Template(TemplateUsage.EnumSelectOne, "Qual das opções abaixo você deseja?")]
+    public class Exemplo
+    {
+        [Prompt("Qual {&} você deseja?{||}", ChoiceFormat ="{1}")]
+        public Salgadinhos Salgadinho { get; set; }
+        //[Prompt("O que deseja beber?")]
+        public Bebidas Bebidas { get; set; }
+        //[Prompt("Selecione o tipo de entrega, motoboy será adiciona R$2,00 ao valor final")]
+        public TipoEntrega TipoEntrega { get; set; }
+        public CPFNaNota CPFNaNota { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+
+        
+        public static IForm<Exemplo> BuildForm()
+        {
+
+            return new FormBuilder<Exemplo>()
+                 .Message("Bem vindo!")
+                 .Field(nameof(Salgadinho))
+                 .Field(nameof(Bebidas))
+                 .Field(nameof(TipoEntrega))
+                 .AddRemainingFields() //Adicona os outros campos
+                 .Confirm("{Nome}.<br/> Você escolheu um {Salgadinho} <br/> acompanhado de uma deliciosa {Bebidas}") //Para mudar a mensagem de confirmação
+                 .OnCompletion(async (c, p) =>
+                 {
+                     await c.PostAsync("Pedido sendo processado");
+                 })
+                 .Build();
+
+        }
+
+        
+    }
+    #region Pedidos
+    [Describe("Tipo de Entrega")]
+    public enum TipoEntrega
+    {
+        [Terms("Retirar No Local", "Passo ai", "Eu pego", "Retiro ai")]
+        [Describe("Retirar No Local")]
+        RetirarNoLocal = 1,
+
+        [Terms("Motoboy", "Motoca", "Cachorro Loco", "Entrega", "Em casa")]
+        [Describe("Motoboy")]
+        Motoboy
+    }
+
+    [Describe("Salgados")]
+    public enum Salgadinhos
+    {
+        [Terms("Esfirra", "Isfirra", "Esfira", "Isfira", "i")]
+        [Describe("Esfirra")]
+        Esfirra = 1,
+
+        [Terms("Quibe", "Kibe", "k", "q")]
+        [Describe("Quibe")]
+        Quibe,
+
+        [Terms("Coxinha", "Cochinha", "Coxa", "c")]
+        [Describe("Coxinha")]
+        Coxinha
+    }
+
+    [Describe("Bebidas")]
+    public enum Bebidas
+    {
+        [Terms("Água", "agua", "h2o", "a")]
+        [Describe("Água")]
+        Agua = 1,
+
+        [Terms("Refrigerante", "refri", "r")]
+        [Describe("Refrigerante")]
+        Refrigerante,
+
+        [Terms("Suco", "s")]
+        [Describe("Suco")]
+        Suco
+    }
+
+    [Describe("CPF na Nota")]
+    public enum CPFNaNota
+    {
+        [Terms("Sim", "s", "yep")]
+        [Describe("Sim")]
+        Sim = 1,
+
+        [Terms("Não", "n", "nao")]
+        [Describe("Não")]
+        Nao
+    }
+    #endregion
+
+
+}
